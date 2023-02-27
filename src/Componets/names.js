f3caf4e4ad982f848a41d582c080eeace54030d4
Kyle Hammersmith <kylehammersmith@Kyles-MacBook-Pro.local>
2023-02-27 14:22:35 -0500

    added my name to the list

--- src/Componets/names.js
+++ src/Componets/names.js
@@ -5,11 +5,13 @@ function Names(){
 // Make a variable add your name to the list  
 
 const name1 = "Howard Henson"
+const name2 = "Kyle Hammersmith"
 
   return(
     <div>
 <ul>
 <li>{name1}</li>
+<li>{name2}</li>
 
 </ul>
 </div>
