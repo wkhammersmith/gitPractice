e76b09a2c30251ff0a442ded1278dc940abb4d28
Jbetrand10 <Joshua.betrand@gmail.com>
2023-03-01 19:21:54 -0500

    Josh's first commit to gitPractice

--- src/Componets/names.js
+++ src/Componets/names.js
@@ -8,6 +8,7 @@ function Names() {
   const name2 = "Kyle Hammersmith"
   const name3 = "Christina Foster"
   const name4 = "Samuel Akins"
+  const name5 = "Joshua Betrand"
 
   return (
     <div>
@@ -16,6 +17,7 @@ function Names() {
         <li>{name2}</li>
         <li>{name3}</li>
         <li>{name4}</li>
+        <li>{name5}</li>
       </ul>
     </div>
 
